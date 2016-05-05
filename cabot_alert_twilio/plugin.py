190f9bd1afe80a46bdb5ae8e164afac463479517
Jonathan Balls <j.balls@btopenworld.com>
2016-05-05 02:53:31 +0100

    Fixed some old stuff

--- cabot_alert_twilio/plugin.py
+++ cabot_alert_twilio/plugin.py
@@ -1,7 +1,6 @@
 from os import environ as env
 
 from django.conf import settings
-from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.template import Context, Template
 from django import forms
@@ -11,17 +10,39 @@ from twilio import twiml
 import requests
 import logging
 
-from cabot.plugins.models import AlertPlugin
+from cabot.plugins.models import AlertPlugin, AlertPluginModel
 
 telephone_template = "This is an urgent message from Arachnys monitoring. Service \"{{ service.name }}\" is erroring. Please check Cabot urgently."
 sms_template = "Service {{ service.name }} {% if service.overall_status == service.PASSING_STATUS %}is back to normal{% else %}reporting {{ service.overall_status }} status{% endif %}: {{ scheme }}://{{ host }}{% url 'service' pk=service.id %}"
 
 logger = logging.getLogger(__name__)
 
-class TwilioPhoneCall(AlertPlugin):
+# We will attach this to TwilioPhoneCallAlert but it will be accessed
+# by both plugins
+class TwilioUserSettingsForm(forms.Form):
+    name = "Twilio Plugin"
+    phone_number = forms.CharField(max_length=30)
+
+    def clean(self, *args, **kwargs):
+	phone_number = self.cleaned_data['phone_number']
+        if not phone_number.startswith('+'):
+            self.cleaned_data['phone_number'] = '+{}'.format(phone_number)
+        return super(TwilioUserSettingsForm, self).clean()
+
+
+class TwilioPhoneCallAlert(AlertPlugin):
     name = "Twilio Phone Call"
     slug = "twilio_phone"
     author = "Jonathan Balls"
+    version = "0.0.1"
+
+    plugin_variables = [
+        'TWILIO_ACCOUNT_SID',
+	'TWILIO_AUTH_TOKEN',
+	'TWILIO_OUTGOING_NUMBER'
+    ]
+
+    user_config_form = TwilioUserSettingsForm
 
     def send_alert(self, service, users, duty_officers):
 
@@ -37,8 +58,8 @@ class TwilioPhoneCall(AlertPlugin):
         client = TwilioRestClient(
             account_sid, auth_token)
 
-        mobiles = TwilioUserData.objects.filter(user__user__in=duty_officers)
-        mobiles = [m.prefixed_phone_number for m in mobiles if m.phone_number]
+	mobiles = [self.plugin_model.get_user_variable(u, 'phone_number') for u in users]
+
         for mobile in mobiles:
             try:
                 client.calls.create(
@@ -50,10 +71,18 @@ class TwilioPhoneCall(AlertPlugin):
             except Exception, e:
                 logger.exception('Error making twilio phone call: %s' % e)
 
-class TwilioSMS(AlertPlugin):
+
+class TwilioSMSAlert(AlertPlugin):
     name = "Twilio SMS"
     slug = "twilio_sms"
     author = "Jonathan Balls"
+    version = "0.0.1"
+
+    plugin_variables = [
+        'TWILIO_ACCOUNT_SID',
+	'TWILIO_AUTH_TOKEN',
+	'TWILIO_OUTGOING_NUMBER'
+    ]
 
     def send_alert(self, service, users, duty_officers):
 
@@ -65,8 +94,8 @@ class TwilioSMS(AlertPlugin):
 
         client = TwilioRestClient(
             account_sid, auth_token)
-        mobiles = TwilioUserData.objects.filter(user__user__in=all_users)
-        mobiles = [m.prefixed_phone_number for m in mobiles if m.phone_number]
+	twilio_phone_plugin = AlertPluginModel.objects.get(slug='twilio_phone')
+	mobiles = [twilio_phone_plugin.get_user_variable(u, 'phone_number') for u in users]
         c = Context({
             'service': service,
             'host': settings.WWW_HTTP_HOST,
@@ -83,15 +112,3 @@ class TwilioSMS(AlertPlugin):
             except Exception, e:
                 logger.exception('Error sending twilio sms: %s' % e)
 
-class TwilioUserData(forms.Form):
-    name = "Twilio Plugin"
-    phone_number = forms.CharField(max_length=30)
-
-    def save(self, *args, **kwargs):
-        if str(self.phone_number).startswith('+'):
-            self.phone_number = self.phone_number[1:]
-        return super(TwilioUserData, self).save(*args, **kwargs)
-
-    @property
-    def prefixed_phone_number(self):
-        return '+%s' % self.phone_number
